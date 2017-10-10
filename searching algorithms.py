291da58505ba622416e4c7b08bb126ad26a29113
yashz1997 <32641195+yashz1997@users.noreply.github.com>
2017-10-10 19:34:58 +0530

    Update searching algorithms.py

--- searching algorithms.py
+++ searching algorithms.py
@@ -6,28 +6,41 @@ Created on Mon Oct  9 17:25:36 2017
 """
 class algo(object):
     
-    def insertionSearch(self):
-      pass  
+    
+    def linearSearch(self,no,myList):
+        count = 0 
+        for x in myList:
+            if no==x:
+                return count
+            count += 1
     
     def binarySearch(self,no,myList):
-        self.found=False
-        self.bottom=0
-        self.top=len(myList)-1
+        found=False
+        bottom=0
+        top=len(myList)-1
     
-        while self.bottom <= self.top and self.found==False:
-            self.middle=(self.bottom+self.top)//2
-            if no==myList[self.middle]:
-                return self.middle
-            elif self.middle<no:
-                self.bottom=self.middle+1
-            elif self.middle>no:
-                self.top=self.middle-1
+        while bottom <= top and found==False:
+            middle=(bottom+top)//2
+            if no==myList[middle]:
+                return middle
+            elif myList[middle]<no:
+                bottom=middle+1
+            elif myList[middle]>no:
+                top=middle-1
     
-    def interpolationSearch(self):
-      pass
-    
-    def hashTable(self):
-      pass
+    def interpolationSearch(self,no,myList,n):
+        
+        bottom = 0
+        top = n-1
+        print(top)
+        while bottom<=top and no>=myList[bottom] and no<=myList[top]:
+            pos = bottom + int(((float(top-bottom) / (myList[top] -myList[bottom])) * (no - myList[bottom])))
+            if no==myList[pos]:
+                return pos
+            elif myList[pos]<no:
+                bottom=pos+1
+            elif myList[pos]>no:
+                top=pos-1
 
 
 
@@ -38,29 +51,43 @@ if __name__=="__main__":
     
     
     while flag==True:
-        print("1. LINEAR SEARCH \n2.BINARY SEARCH \n3.INTERPOLATION SEARCH \n4.HASH TABLE \n5.EXIT\n")
+        
+        print("1.LINEAR SEARCH \n2.BINARY SEARCH \n3.INTERPOLATION SEARCH  \n4.EXIT\n")
         a= int(input("Enter number for using algorithm you want to apply:- "))
+        
+        if a!=5:
+            l=[int(x) for x in input("Input Numbers seperated by comma: ").split(",")]      
+            no= int(input("Enter number you want to search : "))
+            
         if a==1:
-            b.insertionSearch()
+            pos = b.linearSearch(no,l)
+            print(pos)
+            if pos!= "NULL":
+                print("Position of your item is %s" %pos)
+            else:
+                print("No such item found in your list")
+            
             
         elif a==2:
             
-            l=[3,5,8,56,66,85]
-            no=int(input("Enter number you want to search: "))
-            pos=b.binarySearch(no,l)
-            if pos:
-                print("Position of your item is %s"%pos)
+            pos = b.binarySearch(no,l)
+            print(pos)
+            if pos!= "NULL":
+                print("Position of your item is %s" %pos)
             else:
                 print("No such item found in your list")
                     
             
         elif a==3:
-            b.interpolationSearch()
+            length = len(l)
+            pos = b.interpolationSearch(no,l,length)
+            print(pos)
+            if pos!= "NULL":
+                print("Position of your item is %s" %pos)
+            else:
+                print("No such item found in your list")
         
         elif a==4:
-            b.hashTable()
-            
-        elif a==5:
             exit()
         
         else:   
