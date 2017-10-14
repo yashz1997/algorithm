d601f87e64797b09076d3c16da1acd1a92012c8e
yashz1997 <32641195+yashz1997@users.noreply.github.com>
2017-10-14 15:00:44 +0530

    Update organize data.py

--- organize data.py
+++ organize data.py
@@ -6,64 +6,64 @@ Created on Mon Oct  9 17:25:36 2017
 @author: Yash.Zalavadia
 """
 class stack_queue_algo(object):
-    
+    l=[]
     def stack(self):
       flag = True
       while flag:
           print("1. Enter Element 2.Delete Element 3.Display Stack\n")
           a=int(input("Enter your choice: "))
           if a==1:
-            b.push_stack()
+            b.push_stack(self.l)
           if a==2:
-            b.pop()
+            b.pop(self.l)
           if a==3:
             b.display()
 
     def queue(self):
         flag = True
         while flag:
-          print("1. Enter Element 2.Delete Element 3.Display Stack\n")
+          print("1. Enter Element 2.Delete Element 3.Display Queue\n")
           a=int(input("Enter your choice: "))
           if a==1:
-            b.push_queue()
+            b.push_queue(self.l)
           if a==2:
-            b.pop()
+            b.pop(self.l)
           if a==3:
             b.display()
             
     def push_stack(self,l):
-        self.element=int(input("Enter number you want to add to stack: "))
-        l.append(self.element)
+        element=int(input("Enter number you want to add to stack: "))
+        l.append(element)
         
     def push_queue(self,l):
-        self.element=int(input("Enter number you want to add : "))
-        l.insert(0,self.element)
+        element=int(input("Enter number you want to add : "))
+        l.insert(0,element)
         
     def pop(self,l):
         if len(l)==0:
             print("The list is already Empty")
             return
-        l.pop()
+        self.l =l[0:-1] 
         
-    def display(self,l):
-        return l
+    def display(self):
+        print(self.l)
     
 class linkedList:
      def singlyLinkedList(self):
       pass  
     
-    def doublyLinkedList(self):
+     def doublyLinkedList(self):
       pass
     
 if __name__=="__main__":
     
     b = stack_queue_algo()
     l = linkedList()
+    print("1.STACK \n2.QUEUE \n3.SINGLY LINKED LIST \n4.DOUBLY LINKED LIST \n5.EXIT\n")
+    a = int(input("Enter number for using DATA STRUCTURE you want to apply:- "))
     
     while a!=5:
-        print("1. STACK \n2.QUEUE \n3.SINGLY LINKED LIST \n4.DOUBLY LINKED LIST \n5.EXIT\n")
-        a = int(input("Enter number for using DATA STRUCTURE you want to apply:- "))
-    
+        
         if a==1:
             b.stack()
             
