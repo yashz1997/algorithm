4e3a53a87f7ea7ce0a1b008377b01f0849443fc8
yashz1997 <32641195+yashz1997@users.noreply.github.com>
2017-10-24 19:31:02 +0530

    Update shorting algorithms.py

--- shorting algorithms.py
+++ shorting algorithms.py
@@ -42,9 +42,41 @@ class algo(object):
    	        i =i+1	       
         return myList
     
-    def mergeSort(self):
-      pass
-    
+    def mergeSort(alist):                         // Not Understood
+	print("Splitting ",alist)
+	if len(alist)>1:
+	    mid = len(alist)//2
+	    lefthalf = alist[:mid]
+	    righthalf = alist[mid:]
+	
+	    mergeSort(lefthalf)
+	    mergeSort(righthalf)
+
+	    i=0
+	    j=0
+	    k=0
+	    print(alist)
+	    while i < len(lefthalf) and j < len(righthalf):
+	        if lefthalf[i] < righthalf[j]:
+		    alist[k]=lefthalf[i]
+		    i=i+1
+		else:
+		    alist[k]=righthalf[j]
+		    j=j+1
+		    k=k+1
+
+	    while i < len(lefthalf):
+		    alist[k]=lefthalf[i]
+		    i=i+1
+		    k=k+1
+
+	    while j < len(righthalf):
+		    alist[k]=righthalf[j]
+		    j=j+1
+		    k=k+1
+	    print("Merging ",alist)
+	    print(alist)    
+		
     def shellSort(self):
       pass
     
@@ -93,7 +125,7 @@ if __name__=="__main__":
             print(b.selectionSort(l))
         
         elif a==4:
-            b.mergeSort()
+            print(b.mergeSort(l))
             
         elif a==5:
             b.shellSort()
