d13aaa5d073c35dec800ef3e2ee41b7321ea6015
yashz1997 <32641195+yashz1997@users.noreply.github.com>
2017-10-24 14:49:17 +0530

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -7,26 +7,39 @@ Created on Mon Oct  9 17:25:36 2017
 """
 class algo(object):
     
-    def dfs(self):
-      pass
+    def dfs(self,graph, start, end, path=[]):
+        path = path + [start] 
+        if start == end: 
+            return path 
+        for node in graph[start]: 
+            if node not in path: 
+                newpath = find_path(graph, node, end, path) 
+                if newpath: return newpath 
+        return None
     
     def bfs(self):
       pass
 
     def dijkstra(self):
-      pass  
-    
-    
+      pass   
+
+find_path(graph, "A", "D")
     
 if __name__=="__main__":
     
     b = algo()
     print("1.DFS \n2.BFS \n3.DIJKSTRA \n4.EXIT\n")
     a= int(input("Enter number for using algorithm you want to apply:- "))
-    
+    graph = {'A': ['B', 'C'], 
+              'B': ['F'], 
+              'C': ['D'], 
+              'D': ['C'], 
+              'E': ['F'], 
+              'F': ['E']} 
+
     while a!=4:
         if a==1:
-            b.dfs()
+            print(b.dfs(graph,"A","D"))
             
         elif a==2:
             b.bfs()
@@ -39,4 +52,7 @@ if __name__=="__main__":
         
         else:   
             print("Incorrect Input, Please enter correct input")
+            
+           
+        
         
